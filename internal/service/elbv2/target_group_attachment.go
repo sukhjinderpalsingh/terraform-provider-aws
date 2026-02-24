b02abafd7ccbc467c36922d7023bdf4001a29399
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-02-24 16:06:03 -0500

    Use 'sdkid' import alias - elbv2.

--- internal/service/elbv2/target_group_attachment.go
+++ internal/service/elbv2/target_group_attachment.go
@@ -14,7 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
+	sdkid "github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs"
@@ -101,7 +101,7 @@ func resourceAttachmentCreate(ctx context.Context, d *schema.ResourceData, meta 
 	}
 
 	//lintignore:R016 // Allow legacy unstable ID usage in managed resource
-	d.SetId(id.PrefixedUniqueId(targetGroupARN + "-"))
+	d.SetId(sdkid.PrefixedUniqueId(targetGroupARN + "-"))
 
 	return diags
 }
