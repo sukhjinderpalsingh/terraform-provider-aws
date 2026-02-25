d457982a3c28cae47b7a593abbf1468e87c0b59d
Graham Davison <gdavison@hashicorp.com>
2026-02-25 13:28:34 -0800

    Adds `aws_lb` List Resource

--- internal/service/elbv2/service_package_gen.go
+++ internal/service/elbv2/service_package_gen.go
@@ -7,6 +7,8 @@ package elbv2
 
 import (
 	"context"
+	"iter"
+	"slices"
 	"unique"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -288,6 +290,21 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			Region:   unique.Make(inttypes.ResourceRegionDefault()),
 		},
 	}
+}
+
+func (p *servicePackage) SDKListResources(ctx context.Context) iter.Seq[*inttypes.ServicePackageSDKListResource] {
+	return slices.Values([]*inttypes.ServicePackageSDKListResource{
+		{
+			Factory:  newLoadBalancerResourceAsListResource,
+			TypeName: "aws_lb",
+			Name:     "Load Balancer",
+			Region:   unique.Make(inttypes.ResourceRegionDefault()),
+			Tags: unique.Make(inttypes.ServicePackageResourceTags{
+				IdentifierAttribute: names.AttrARN,
+			}),
+			Identity: inttypes.RegionalARNIdentity(),
+		},
+	})
 }
 
 func (p *servicePackage) ServicePackageName() string {
