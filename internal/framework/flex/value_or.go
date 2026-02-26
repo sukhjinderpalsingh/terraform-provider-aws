7e45bf0f0e7d466281c11bf633bbb09cc8284c90
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-02-26 12:54:27 -0500

    a/aws_lambda_invoke: Use helpers in 'framework/actions'.

--- internal/framework/flex/value_or.go
+++ internal/framework/flex/value_or.go
@@ -11,6 +11,9 @@ import (
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
 )
 
+// *ValueOr returns the value if it is not null or unknown, otherwise it returns the provided default value.
+// Useful for filling in default values when no plan has is available.
+
 func Int64ValueOr(ctx context.Context, v types.Int64, defaultValue int64) int64 {
 	if v.IsNull() || v.IsUnknown() {
 		return defaultValue
